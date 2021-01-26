eb1276c4b0ec75b5c4ff5be0002019587c5fdd04
Henning Dieterichs <henning.dieterichs@live.de>
2021-01-26 16:45:43 +0100

    Adds koa server.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 import { Search, TwitterClient } from "twitter-api-client";
 import { Disposable } from "@hediet/std/disposable";
 import { EventEmitter } from "@hediet/std/events";
+import * as Koa from "koa";
+import * as Router from "koa-router";
+import * as json from "koa-json";
+
+class Server {
+	constructor() {
+		const app = new Koa();
+		const router = new Router();
+
+		router.get("/", async (ctx, next) => {
+			ctx.body = { status: "ok" };
+			await next();
+		});
+
+		app.use(json());
+		app.use(router.routes()).use(router.allowedMethods());
+
+		app.listen(8080, () => {
+			console.log("Server listening on port 8080");
+		});
+	}
+}
 
 class Main {
 	private readonly twitterClient = new TwitterClient({
@@ -11,6 +33,8 @@ class Main {
 	});
 
 	private readonly friendsCache = new FriendsCache(this.twitterClient);
+
+	private readonly server = new Server();
 
 	constructor() {
 		this.run();
