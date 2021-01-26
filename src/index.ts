a86e72077831a18babcb74664491cfce0ee3809f
Henning Dieterichs <henning.dieterichs@live.de>
2021-01-26 16:07:41 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,173 @@
+import { Search, TwitterClient } from "twitter-api-client";
+import { Disposable } from "@hediet/std/disposable";
+import { EventEmitter } from "@hediet/std/events";
+import * as local from "../local.config.json";
+
+class Main {
+	private readonly twitterClient = new TwitterClient({
+		apiKey: local.apiKey,
+		apiSecret: local.apiSecret,
+		accessToken: local.accessToken,
+		accessTokenSecret: local.accessTokenSecret,
+	});
+
+	private readonly friendsCache = new FriendsCache(this.twitterClient);
+
+	constructor() {
+		this.run();
+	}
+
+	async run() {
+		const screenName = (
+			await this.twitterClient.accountsAndUsers.accountSettings()
+		).screen_name;
+		const myUserInfo = await this.twitterClient.accountsAndUsers.usersShow({
+			screen_name: screenName,
+		});
+
+		let lastPostTimestamp: number = 0; // in ms
+
+		if (myUserInfo.status) {
+			lastPostTimestamp = new Date(
+				myUserInfo.status.created_at
+			).getTime();
+		}
+
+		console.log(
+			`Using screen name "${screenName}" (id "${myUserInfo.name}")`
+		);
+
+		const searchListener = new SearchListener(this.twitterClient, [
+			"#StarshipLaunchIn5min",
+			"#StarshipLaunchIn10min",
+			"#StarshipLaunchIn15min",
+			"#StarshipLaunchIn20min",
+			"#StarshipLaunchIn25min",
+			"#StarshipLaunchIn30min",
+		]);
+
+		searchListener.onTweet.sub(async (t) => {
+			if (t.status.retweeted) {
+				// It must be an original tweet.
+				return;
+			}
+
+			if (!(await this.friendsCache.isFriend(t.userId))) {
+				return;
+			}
+
+			if (new Date().getTime() - t.createdAt.getTime() > 30 * 60 * 1000) {
+				// this tweet is too old
+				console.log(
+					`Ignoring tweet by ${t.status.user.screen_name} ("${t.status.user.name}") as it is too old.`
+				);
+				return;
+			}
+
+			if (new Date().getTime() - lastPostTimestamp > 60 * 60 * 1000) {
+				console.log(
+					`Ignoring tweet by ${t.status.user.screen_name} ("${t.status.user.name}") as we already posted about the launch.`
+				);
+				return;
+			}
+
+			console.log(
+				`Retweeting tweet by ${t.status.user.screen_name} ("${t.status.user.name}")!`
+			);
+
+			lastPostTimestamp = new Date().getDate();
+			await this.twitterClient.tweets.statusesRetweetById({ id: t.id });
+		});
+	}
+}
+
+class Tweet {
+	public get id(): string {
+		return this.status.id_str;
+	}
+	public get userId(): string {
+		return this.status.user.id_str;
+	}
+	public get createdAt(): Date {
+		return new Date(this.status.created_at);
+	}
+
+	constructor(public readonly status: Search["statuses"][0]) {}
+}
+
+class SearchListener {
+	public readonly dispose = Disposable.fn();
+
+	private sinceIdStr: string | undefined;
+
+	private readonly _onTweet = new EventEmitter<Tweet>();
+	public readonly onTweet = this._onTweet.asEvent();
+
+	constructor(
+		private readonly twitterClient: TwitterClient,
+		private readonly tags: string[]
+	) {
+		setInterval(() => {
+			this.run();
+		}, (15 * 60 * 1000) / 60); // rate limit: 75 times per 15 minutes
+		this.run();
+	}
+
+	private async run(): Promise<void> {
+		const result = await this.twitterClient.tweets.search({
+			q: this.tags.join(" OR "),
+			include_entities: false,
+			since_id: this.sinceIdStr,
+			result_type: "recent",
+		});
+
+		for (const s of result.statuses) {
+			if (s.id_str === this.sinceIdStr) {
+				continue;
+			}
+
+			const t = new Tweet(s);
+			this._onTweet.emit(t);
+		}
+
+		this.sinceIdStr = result.search_metadata.max_id_str;
+	}
+}
+
+class FriendsCache {
+	private cachedFriendIds = new Set<string>();
+	private cachedFriendsTimestamp: number = 0; /* in ms */
+	private cacheUpdate: Promise<void> | undefined;
+
+	constructor(private readonly twitterClient: TwitterClient) {}
+
+	public async isFriend(id: string): Promise<boolean> {
+		const time = new Date().getTime();
+		if (time - this.cachedFriendsTimestamp > 30 * 1000) {
+			this.cachedFriendsTimestamp = time;
+
+			// Refresh cache every 30 seconds.
+			this.cacheUpdate = this.fetchFriends();
+		}
+		if (this.cacheUpdate) {
+			await this.cacheUpdate;
+		}
+		return this.cachedFriendIds.has(id);
+	}
+
+	private async fetchFriends() {
+		try {
+			const friends = await this.twitterClient.accountsAndUsers.friendsList(
+				{
+					count: 500,
+					include_user_entities: false,
+				}
+			);
+			this.cachedFriendIds = new Set(friends.users.map((u) => u.id_str));
+		} catch (e) {
+			console.error("Fetch friends failed: ", e);
+		}
+	}
+}
+
+new Main();
